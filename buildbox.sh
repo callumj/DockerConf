cd91d69964599bb2beb743e9b493f42be30308fa
Callum Jones <callum@callumj.com>
2014-05-11 20:16:21 +0800

    Build as needed

---
+++ buildbox.sh
@@ -0,0 +1,7 @@
+curl "https://api.buildbox.io/v1/projects/callumj/dockerconf/builds?api_key=$BUILDBOX_API_KEY" \
+-X POST \
+-F "commit=master" \
+-F "branch=master" \
+-F "author[name]=Callum Jones" \
+-F "author[email]=push@callumj.com" \
+-F "message=Update push"
