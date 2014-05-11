b9a8c040bf79541c0c726d4c7625e11701864d35
Callum Jones <callum@callumj.com>
2014-05-11 21:29:57 +0800

    Just debug

--- buildbox.sh
+++ buildbox.sh
@@ -1,8 +1,1 @@
 export
-curl "https://api.buildbox.io/v1/projects/callumj/weave_projects/builds?api_key=$BUILDBOX_API_KEY" \
--X POST \
--F "commit=master" \
--F "branch=master" \
--F "author[name]=Callum Jones" \
--F "author[email]=push@callumj.com" \
--F "message=Update push" 
