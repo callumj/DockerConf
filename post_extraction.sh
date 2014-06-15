c4b20536fc63d2ae1e460c6b7645bf4cb32102d3
Callum Jones <callum@callumj.com>
2014-06-15 22:17:07 +0800

    Busan now depends on after weavefiles

--- post_extraction.sh
+++ post_extraction.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 for BaseName in *; do
-  if [ -f "${BaseName}/Dockerfile" ]
+  if [[ -f "${BaseName}/Dockerfile" && ! "${BaseName}" =~ "busan" ]]
   then
     busan "${BaseName}"
   fi
 done
+
+if [[ -f "busanfiles-deploy/Dockerfile" ]]
+then
+  busan busanfiles-deploy
+fi
