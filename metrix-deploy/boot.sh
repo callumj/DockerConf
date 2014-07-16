4442b04dabdaf1f449e36759a90f0570a62754ca
Callum Jones <callum@callumj.com>
2014-07-16 20:32:41 +0800

    Corrections

--- metrix-deploy/boot.sh
+++ metrix-deploy/boot.sh
@@ -8,6 +8,4 @@ go get -u github.com/callumj/metrix
 
 redis-server /etc/redis.conf
 
-curl -X POST "http://metrix.callumj.com/version?source=docker&key=$NAME&version=$VERSION"
-
 /opt/go/bin/metrix /etc/metrix.yml
